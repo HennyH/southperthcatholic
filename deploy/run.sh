e3b9fdd99648bfa44f896cdb401451ff18a1244b
Henry Hollingworth <hollingworth.henry@gmail.com>
2021-04-02 09:16:32 +0800

    change mass time

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,2 +1,2 @@
 docker build -t southperthcatholic . \
-    && docker run -v bulletins:/app/bulletins -p 8080:5000 southperthcatholic gunicorn --bind 0.0.0.0:5000 "app:main(['--admin-password', '$ADMIN_PASSWORD'])"
+    && docker run -p 8080:5000 southperthcatholic gunicorn --bind 0.0.0.0:5000 "app:main(['--admin-password', '$ADMIN_PASSWORD'])"
