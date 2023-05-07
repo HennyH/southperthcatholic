fa24fc9d5a84fe1f01521bc2dfda92109e6e8794
Henry Hollingworth <henry.hollingworth@alcoa.com>
2023-05-07 11:22:32 +0800

    (admin) update photos

--- src/_data/photos.js
+++ src/_data/photos.js
@@ -1,19 +1,19 @@
 const fs = require("node:fs/promises");
 
-module.exports = async function() {
-    const folders = {
-        photos: "assets/images/photos",
-        saints: "assets/images/saints",
-        solemnities: "assets/images/solemnities",
-    }
+module.exports = async function () {
+  const folders = {
+    photos: "assets/images/photos",
+    saints: "assets/images/saints",
+    solemnities: "assets/images/solemnities",
+  };
 
-    const photos = {};
+  const photos = {};
 
-    for (const name in folders) {
-        const path = folders[name];
-        const files = await fs.readdir(path);
-        photos[name] = files.map(n => `${path}/${n}`);
-    }
+  for (const name in folders) {
+    const path = folders[name];
+    const files = await fs.readdir(path);
+    photos[name] = files.map((n) => `${path}/${n}`);
+  }
 
-    return photos;
-}
+  return photos;
+};
